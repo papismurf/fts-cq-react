2b999095d7eb349a9888aabfb51e3c9fbcf2e0f2
David James Brown <browndj24@googlemail.com>
2018-12-19 10:26:10 +0000

    added 404 Handling

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import Questionnaire from "./components/Questionnaire";
+import NotFound from "./components/NotFound";
 
 class Routes extends Component {
     render() {
@@ -11,6 +12,7 @@ class Routes extends Component {
                 <Route exact path="/" component={Home}/>
                 <Route path="/login" exact component={Login}/>
                 <Route path="/questionnaire" exact component={Questionnaire}/>
+                <Route component={NotFound}/>
             </Switch>
         );
     }
